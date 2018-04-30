74f44c7aff4bebe3020e77d56854f9a57792b5ce
Karan Murthy <karanmurthy7.applications@gmail.com>
2018-04-30 13:59:04 -0700

    simple feed forward network

--- feedforward-net.py
+++ feedforward-net.py
@@ -7,11 +7,12 @@ from torch.backends.cudnn.rnn import _hidden_size
 batch_size = 5
 input_size = 3
 hidden_size = 8
-num_classes = 2
+num_classes = 4
 
 torch.manual_seed(123)
 input = autograd.Variable(torch.rand(batch_size, input_size))
-print('input', input)
+target = autograd.Variable((torch.rand(batch_size) * num_classes).long())
+
 
 class Net(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
@@ -23,8 +24,13 @@ class Net(nn.Module):
         x = self.h1(x)
         x = F.tanh(x)
         x = self.h2(x)
+        x = F.softmax(x)
         return x
     
 model = Net(input_size=input_size, hidden_size=hidden_size, num_classes=num_classes)
 output = model(input)
 print('out', output)
+_, pred = output.max(1)
+
+print('target : ', target.view(1, -1))
+print('pred : ', pred.view(1, -1))
